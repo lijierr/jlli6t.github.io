4ee711825bfcd9974d56b8dfb439910c5b3213ce
jlli6 <jie.li6@uq.net.au>
2020-06-17 19:03:33 +1000

    test for pypi

--- setup.py
+++ setup.py
@@ -8,18 +8,17 @@ with open('README.md') as fh:
 	long_description = fh.read()
 
 setup(
-	name = 'test_package',
-	version = '1.0',
+	name = 'test_pypi',
+	version = '2.0',
 	description = 'this is test for packing a package.',
 	long_description = long_description,
 	long_description_content_type = 'text/markdown',
-	install_required=[None],
 	url='https://github.com/jlli6t/test_pypi',
 	author = 'M.M Jie Li',
 	author_email = 'mm.jlli6t@gmail.com',
 	packages = find_packages(),
 	install_required = ['numpy==1.14.4'],
-	entry_points={'console_scripts':['test_package=test_package1:info1'],},
+	entry_points={'console_scripts':['test_pypi=test_package1:info1'],},
 	classifiers = (
 		"Programming Language :: Python ::3",
 		"License :: MIT License",
